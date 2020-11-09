f95223f4e8b2bff292e4d49e4032d4f34f482432
best1650 <35982828+li3946@users.noreply.github.com>
2020-11-09 16:57:28 -0500

    add new file

---
+++ S&P.py
@@ -0,0 +1,20 @@
+import os
+import operator
+import requests
+import urllib.parse
+import textwrap
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
+import datetime
+import time
+import mplcursors
+import json
+import warnings
+import pandas as pd
+
+warnings.filterwarnings("ignore")
+
+if __name__ == "__main__":
+    print('Done!')
